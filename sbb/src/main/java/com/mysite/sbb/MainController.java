91f60dba6216b141a4081710987295b846cd3993
“Donghyeok <qwer5383@naver.com>
2023-04-18 16:50:20 +0900

    ROOT URL

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -12,4 +12,9 @@ public class MainController {
 //		System.out.println("index");
 		return "안녕하세요 sbb에 오신 것을 환영합니다.";
 	}
+	
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list"; // question/list URL로 페이지를 리다이렉트 
+	}
 }
